a9849a171fba2450d1fd29c543730c8c2712ba58
Daniel Lucas <89852516+DanielLucasOliveira@users.noreply.github.com>
2024-08-12 23:17:31 -0300

    fix: changing crontollers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService],
+  controllers: [UserController],
+  providers: [PrismaService, UserService],
 })
 export class AppModule {}
